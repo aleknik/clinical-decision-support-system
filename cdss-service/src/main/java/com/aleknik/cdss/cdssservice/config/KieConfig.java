f409e9ba6562b46371a4b539d70319d68f69dbb3
Aleksandar Nikolic <nikolic95@gmail.com>
2018-06-14 13:50:12 +0200

    Add kie config

---
+++ cdss-service/src/main/java/com/aleknik/cdss/cdssservice/config/KieConfig.java
@@ -0,0 +1,36 @@
+package com.aleknik.cdss.cdssservice.config;
+
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieScanner;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KieConfig {
+
+    @Value("${rules.groupId}")
+    private String groupId;
+
+    @Value("${rules.artifactId}")
+    private String artifactId;
+
+    @Value("${rules.version}")
+    private String version;
+
+    @Value("${rules.sessionName}")
+    private String sessionName;
+
+    @Bean
+    public KieSession kieSession() {
+        final KieServices kieServices = KieServices.Factory.get();
+
+        final KieContainer kieContainer = kieServices.newKieContainer(kieServices.newReleaseId(groupId, artifactId, version));
+        final KieScanner kieScanner = kieServices.newKieScanner(kieContainer);
+        kieScanner.start(10000);
+
+        return kieContainer.newKieSession(sessionName);
+    }
+}
