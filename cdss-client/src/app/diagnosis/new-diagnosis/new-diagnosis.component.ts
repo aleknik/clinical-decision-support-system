b6883570bd553d11c70f1b86e70548fa61687a96
Aleksandar Nikolic <nikolic95@gmail.com>
2018-06-22 01:24:13 +0200

    Diagnose WIP

--- cdss-client/src/app/diagnosis/new-diagnosis/new-diagnosis.component.ts
+++ cdss-client/src/app/diagnosis/new-diagnosis/new-diagnosis.component.ts
@@ -5,6 +5,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Disease } from '../../shared/model/disease.nodel';
 import { DiseaseService } from '../../core/http/disease.service';
+import { Medicine } from '../../shared/model/medicine.model';
+import { MedicineService } from '../../core/http/medicine.service';
 
 @Component({
   selector: 'app-new-diagnosis',
@@ -16,15 +18,19 @@ export class NewDiagnosisComponent implements OnInit {
   patientId: number;
 
   allSymptoms = new Array<Symptom>();
+  allMedicines = new Array<Medicine>();
 
   selectedSymptomName: string;
+  selectedMedicineName: string;
 
   selectedSymptoms = Array<Symptom>();
+  selectedMedicines = Array<Medicine>();
 
   suggestedDiseases = Array<Disease>();
 
   constructor(private symptomService: SymptomService,
     private diseaseService: DiseaseService,
+    private medicineService: MedicineService,
     private route: ActivatedRoute,
     private router: Router,
     private toastr: ToastrService) { }
@@ -33,12 +39,19 @@ export class NewDiagnosisComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.patientId = params['id'];
       this.getAllSymptoms();
+      this.getAllMedicine();
     });
   }
 
   private getAllSymptoms(): void {
     this.symptomService.findAll().subscribe(result => {
       this.allSymptoms = result;
+    });
+  }
+
+  private getAllMedicine(): void {
+    this.medicineService.findAll().subscribe(result => {
+      this.allMedicines = result;
     });
   }
 
@@ -49,11 +62,25 @@ export class NewDiagnosisComponent implements OnInit {
     this.selectedSymptomName = '';
   }
 
+  medicineSelected(event) {
+    if (!this.selectedMedicines.some(x => x.id === event.item.id)) {
+      this.selectedMedicines.push(event.item);
+    }
+    this.selectedMedicineName = '';
+  }
+
   removeSymptom(symptom: Symptom) {
     const index: number = this.selectedSymptoms.indexOf(symptom);
     if (index !== -1) {
       this.selectedSymptoms.splice(index, 1);
       this.suggestedDiseases = [];
+    }
+  }
+
+  removeMedicine(medicine: Medicine) {
+    const index: number = this.selectedMedicines.indexOf(medicine);
+    if (index !== -1) {
+      this.selectedMedicines.splice(index, 1);
     }
   }
 
